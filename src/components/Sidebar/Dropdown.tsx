df0e04a6d68b259672331fcc49319631bc7634ba
snaps-ariel <ariel@snaps.com>
2024-01-23 17:06:18 +0900

    feat: Dropdown 컴포넌트 추가

---
+++ src/components/Sidebar/Dropdown.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+interface IDropdownProps {
+  subMenu: subMenu[];
+  onClick: (e: React.MouseEvent<HTMLLIElement>, link: string) => void;
+}
+
+type subMenu = {
+  id: number;
+  title: string;
+  link: string;
+};
+export default function Dropdown({ subMenu, onClick }: IDropdownProps) {
+  const pathname = usePathname();
+
+  return (
+    <ul className="py-2 space-y-2">
+      {subMenu.map(({ id, title, link }, idx) => (
+        <li key={`${id}-${idx}`} value={link} onClick={(e) => onClick(e, link)}>
+          <Link
+            href={link}
+            className={`flex items-center w-full p-2 transition duration-75 rounded-lg pl-11 group ${
+              pathname === link ? 'text-opmyellow' : 'text-white'
+            } hover:bg-gray-700`}
+          >
+            {title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
